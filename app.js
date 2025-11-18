e9ee2094a7379712f1467a4dd00dee9df36232f0
nelson <nelso.rw@gmail.com>
2025-11-18 08:29:29 +0200

    WEB-1: Add /home route to support homepage feature

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
 const express = require('express');
 const app = express();
 
+// Default route
 app.get('/', (req, res) => {
   res.send('Welcome to the Homepage!');
 });
 
+// NEW homepage route for feature/homepage branch
+app.get('/home', (req, res) => {
+  res.send('This is the official Homepage route!');
+});
+
 const PORT = 3000;
 app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
+
