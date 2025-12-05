43adb3d7d19f0061fdf2e6597a81a58a4890b18c
Nelson <nelson02122002nelson@gmail.com>
2025-12-05 19:43:02 +0200

    WEB-1: Update homepage styling #done

---
+++ main.tf
@@ -0,0 +1,15 @@
+locals {
+  app_code = <<-EOF
+    console.log("Hello from Terraform-generated app!");
+  EOF
+}
+
+resource "local_file" "webapp_app" {
+  content  = local.app_code
+  filename = "${path.module}/webapp/src/index.js"
+}
+
+output "webapp_file_path" {
+  value       = resource.local_file.webapp_app.filename
+  description = "The full path to the web application's index.js file."
+}
